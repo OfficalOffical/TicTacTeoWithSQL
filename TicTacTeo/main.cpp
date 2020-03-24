a1ee1c669157df917b85f67059853b6e8761b446
Sefa <mail@sefakalkan.com>
2020-03-24 20:45:56 +0300

    Update main.cpp

--- TicTacTeo/main.cpp
+++ TicTacTeo/main.cpp
@@ -28,12 +28,4 @@ bool table(int input) {
 }
 
 
-int main() {
-    int a;
-    while(table(a)!= true){
-        std::cout<<"Please chose your place : ";
-        std::cin>>a;
-    }
-    // To prove i can add 1-9 limit to while loop
-    // I can add != X && != O in switch case
 }
