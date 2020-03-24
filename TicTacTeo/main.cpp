9d9d7cbd1f452ea7a071c9f010513e9c4a3d9757
Sefa <mail@sefakalkan.com>
2020-03-24 20:42:31 +0300

    Kod çalışıyor versiyon 2

--- TicTacTeo/main.cpp
+++ TicTacTeo/main.cpp
@@ -11,24 +11,7 @@ bool table(int input) {
                 break;
             case 2:
                 sayac % 2 == 0 ? b = 'X' : b = 'O';
-                break;
-            case 3:
-                sayac % 2 == 0 ? c = 'X' : c = 'O';
-                break;
-            case 4:
-                sayac % 2 == 0 ? d = 'X' : d = 'O';
-                break;
-            case 5:
-                sayac % 2 == 0 ? e = 'X' : e = 'O';
-                break;
-            case 6:
-                sayac % 2 == 0 ? f = 'X' : f = 'O';
-                break;
-            case 7:
-                sayac % 2 == 0 ? g = 'X' : g = 'O';
-                break;
-            case 8:
-                sayac % 2 == 0 ? h = 'X' : h = 'O';
+ 2 == 0 ? h = 'X' : h = 'O';
                 break;
             case 9:
                 sayac % 2 == 0 ? i = 'X' : i = 'O';
@@ -41,19 +24,7 @@ bool table(int input) {
         std::cout << "|     " << "    |     " << "    |     " << "    |" << std::endl;
         std::cout << "-------------------------------" << std::endl;
         std::cout << "|     " << "    |     " << "    |     " << "    |" << std::endl;
-        std::cout << "|    " << d << "    |    " << e << "    |    " << f << "    |" << std::endl;
-        std::cout << "|     " << "    |     " << "    |     " << "    |" << std::endl;
-        std::cout << "-------------------------------" << std::endl;
-        std::cout << "|     " << "    |     " << "    |     " << "    |" << std::endl;
-        std::cout << "|    " << g << "    |    " << h << "    |    " << i << "    |" << std::endl;
-        std::cout << "|     " << "    |     " << "    |     " << "    |" << std::endl;
-        std::cout << "-------------------------------" << std::endl;
 
-          if ((a == b && b == c) || (a == e && e == i) || (a == d && d == g) || (b == e && e == h) || (c == e && e == g) ||(c == f && f == i) || (d == e && e == f) || g == h && h == i) {
-            sayac % 2 == 0 ? std::cout<<"Player 1 won"<<std::endl : std::cout<<"Player 2 won"<<std::endl;
-        return true;
-        }
-        return false;
 }
 
 
