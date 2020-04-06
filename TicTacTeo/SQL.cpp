00d8dfe361ae039636866eb14114fd91ab16ff3e
Sefa <mail@sefakalkan.com>
2020-04-06 22:56:51 +0300

    Add files via upload

---
+++ TicTacTeo/SQL.cpp
@@ -0,0 +1,10 @@
+//
+// Created by sefak on 24.03.2020.
+//
+
+#include "SQL.h"
+
+void SQL::Creator() {
+    MYSQL * conn;
+
+}
---
+++ TicTacTeo/SQL.h
@@ -0,0 +1,20 @@
+//
+// Created by sefak on 24.03.2020.
+//
+
+#ifndef SQLTITTACTEO_SQL_H
+#define SQLTITTACTEO_SQL_H
+
+#include <iostream>
+#include "mysql.h"
+
+
+
+class SQL {
+  static void Creator() ;
+
+
+};
+
+
+#endif //SQLTITTACTEO_SQL_H
