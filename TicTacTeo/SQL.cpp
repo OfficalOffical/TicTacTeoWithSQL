2d3b1aa1b05edf649c9c87936ba5217f1ad96139
Sefa <mail@sefakalkan.com>
2020-04-23 13:05:22 +0300

    Update SQL.cpp

--- TicTacTeo/SQL.cpp
+++ TicTacTeo/SQL.cpp
@@ -1,10 +1,19 @@
-//
-// Created by sefak on 24.03.2020.
-//
+#include "ConMake.h"
 
-#include "SQL.h"
+int ConMake::mainCon(MYSQL * conn)
+{
+    MYSQL_ROW row;
+    MYSQL_RES* res;
 
-void SQL::Creator() {
-    MYSQL * conn;
-
+    conn = mysql_real_connect(conn, "foo", "bar", "foo", "bar", 3306, NULL, 0);
+   
+    if (conn) {
+        cout << "Successfully connected to Database"<<endl;
+        return 1;
+    }
+    else {
+        cout << "Unable to connect Database";
+        return 0;
+    }
 }
+
