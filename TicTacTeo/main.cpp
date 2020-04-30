de59ed1fa3c2716ea85b467986fe90ce60c03894
Sefa <mail@sefakalkan.com>
2020-04-30 16:51:38 +0300

    Update main.cpp

--- TicTacTeo/main.cpp
+++ TicTacTeo/main.cpp
@@ -1,41 +1,76 @@
+// HW.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
 #include "ConMake.h"
+
 #include "HW.h"
 
 bool table(int input);
 int init(int* p);
 
-bool table(int input);
-int init(int* p);
-
+int isEnd = 0;
 int sayac = 0;
 char a = '1', b = '2', c = '3', d = '4', e = '5', f = '6', g = '7', h = '8', i = '9';
 int control[9];
 //If unbounded number entered it brokes game bcs of counter (sayac) can be solved in switch case 
 //Can be upgraded by adding do you want to play again and creating new tables on sql by it
-int main()
-{
-    MYSQL * connect;
+int main() {
+    ConMake conMake;
+    MYSQL* connect;
     connect = mysql_init(0);
     ConMake::mainCon(connect);
     string p1;
-    string p2;
-    
-    init(control);
-    int a = 0;
 
-    while (table(a) != true) {
-        std::cout << "Please chose your place : ";
-        std::cin >> a;
-        if(control[a] != 1 ){
-            if (sayac % 2 != 0) {
-                    p1 = "Insert INTO Game1(Player1)VALUE(" + to_string(a) + ");";                
-                    mysql_query(connect, p1.c_str());
-                   }
-            else {
-                    p2 = "Insert INTO Game1(Player2)VALUE(" + to_string(a) + ");";
-                    mysql_query(connect, p2.c_str());
-    
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int d = 1;
+    init(control);
+
+    std::cout << "What you want to do : \n1)Play the game\n2)Wiew SQL data\n3)Delete data inside the SQL\n4)Exit" << endl;
+    cin >> c;
+    while (d) {
+        switch (c) {
+        case 1:
+            if ((sayac != 9) || (isEnd != 1)) {
+                while (table(a) != true) {
+                    std::cout << "Please chose your place : ";
+                    std::cin >> a;
+                    if (control[a] != 1) {
+                        if (sayac % 2 == 0) { //
+                            p1 = "Insert INTO Game2(FirstPlayer,SecondPlayer)VALUE(" + to_string(b) + "," + to_string(a) + ");"; // when player1 wons it doesnt enter the if so it'll not send data to sql      
+                            mysql_query(connect, p1.c_str());
+                            //Storing values in the array could be solve the problems
+                        }
+                        else {
+                            b = a;
+                        }
+                    }
+
+                }
+                std::cout << "What you want next : \n1)Play the game\n2)Wiew SQL data\n3)Delete data inside the SQL\n4)Exit" << endl;
+                cin >> c;
+                break;
             }
+            else {
+                std::cout << "What you want next : \n1)Play the game\n2)Wiew SQL data\n3)Delete data inside the SQL\n4)Exit" << endl;
+                cin >> c;
+                break;
+            }
+
+        case 2:
+            conMake.showInside(connect);
+            std::cout << "What you want next : \n1)Play the game\n2)Wiew SQL data\n3)Delete data inside the SQL\n4)Exit" << endl;
+            cin >> c;
+            break;
+        case 3:
+            conMake.deleteInside(connect);
+            std::cout << "What you want next : \n1)Play the game\n2)Wiew SQL data\n3)Delete data inside the SQL\n4)Exit" << endl;
+            cin >> c;
+            break;
+        case 4:
+            d = 0;
+            break;
         }
     }
 }
@@ -56,9 +91,11 @@ bool table(int input) {
         if (*p == 0) {
             sayac % 2 == 0 ? a = 'X' : a = 'O';
             *p = 1;
+
         }
         else
             sayac--;
+
         break;
     case 2:
         if (*(p + 1) == 0) {
@@ -67,6 +104,7 @@ bool table(int input) {
         }
         else
             sayac--;
+
         break;
     case 3:
         if (*(p + 2) == 0) {
@@ -75,6 +113,7 @@ bool table(int input) {
         }
         else
             sayac--;
+
         break;
     case 4:
         if (*(p + 3) == 0) {
@@ -83,6 +122,7 @@ bool table(int input) {
         }
         else
             sayac--;
+
         break;
     case 5:
         if (*(p + 4) == 0) {
@@ -91,6 +131,7 @@ bool table(int input) {
         }
         else
             sayac--;
+
         break;
     case 6:
         if (*(p + 5) == 0) {
@@ -99,6 +140,7 @@ bool table(int input) {
         }
         else
             sayac--;
+
         break;
     case 7:
         if (*(p + 6) == 0) {
@@ -107,6 +149,7 @@ bool table(int input) {
         }
         else
             sayac--;
+
         break;
     case 8:
         if (*(p + 7) == 0) {
@@ -116,6 +159,7 @@ bool table(int input) {
         }
         else
             sayac--;
+
         break;
     case 9:
         if (*(p + 8) == 0) {
@@ -123,7 +167,8 @@ bool table(int input) {
             *(p + 8) = 1;
         }
         else
-            sayac--;++
+            sayac--;
+
         break;
     }
 
@@ -143,9 +188,20 @@ bool table(int input) {
 
     if ((a == b && b == c) || (a == e && e == i) || (a == d && d == g) || (b == e && e == h) || (c == e && e == g) ||
         (c == f && f == i) || (d == e && e == f) || g == h && h == i) {
+        isEnd = 1;
         sayac % 2 == 0 ? std::cout << "Player 1 won" << std::endl : std::cout << "Player 2 won" << std::endl; // if things go complex we can make this [3][3] array // it does not end the game at draw
-
         return true;
     }
     return false;
 }
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
